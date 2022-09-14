ec62e0e45d4200a752c28eb117066f1e8ee4f631
vallathn <vallathn@gmail.com>
2022-09-14 14:15:32 -0500

    day 8

---
+++ day7_structs/structs_f22_am/moonVillain.c
@@ -0,0 +1,5 @@
+//
+// Created by Vallath Nandakumar on 9/13/22.
+//
+
+#include "moonVillain.h"
---
+++ day7_structs/structs_f22_am/moonVillain.h
@@ -0,0 +1,8 @@
+//
+// Created by Vallath Nandakumar on 9/13/22.
+//
+
+#ifndef STRUCTS_F22_AM_MOONVILLAIN_H
+#define STRUCTS_F22_AM_MOONVILLAIN_H
+
+#endif //STRUCTS_F22_AM_MOONVILLAIN_H
