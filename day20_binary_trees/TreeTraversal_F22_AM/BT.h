fd648267b512054b604d140a6c3b575b65064d8d
vallathn <vallathn@gmail.com>
2022-11-02 18:55:58 -0500

    day 21 maps

---
+++ day20_binary_trees/TreeTraversal_F22_AM/BT.h
@@ -0,0 +1,16 @@
+//
+// Created by Vallath Nandakumar on 11/1/22.
+//
+
+#ifndef TREETRAVERSAL_F22_AM_BT_H
+#define TREETRAVERSAL_F22_AM_BT_H
+
+
+
+class BT {
+
+};
+
+
+
+#endif //TREETRAVERSAL_F22_AM_BT_H
---
+++ day20_binary_trees/TreeTraversal_F22_PM/BT.h
@@ -0,0 +1,14 @@
+//
+// Created by Vallath Nandakumar on 11/1/22.
+//
+
+#ifndef TREETRAVERSAL_F22_PM_BT_H
+#define TREETRAVERSAL_F22_PM_BT_H
+
+
+class BT {
+
+};
+
+
+#endif //TREETRAVERSAL_F22_PM_BT_H
